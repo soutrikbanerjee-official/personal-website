cfffb9a3b3d0341794adf63bd4e4342e1b579a22
Soutrik Banerjee <150548253+soutrikbanerjee-official@users.noreply.github.com>
2026-08-06 18:38:38 +0000

    Update 26.0.21

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -1,9 +1,22 @@
-document.getElementById("footer").innerHTML = `<ul id="footernav">
+document.getElementById("footer").innerHTML =
+`
+<ul id="footernav">
             <li class="footnavlink"><a class="footnavlink" href="index.html">Home</a></li>
             <li class="footnavlink"><a class="footnavlink" href="about.html">About Me</a></li>
             <li class="footnavlink"><a class="footnavlink" href="projects.html">Projects</a></li>
             <li class="footnavlink"><a class="footnavlink" href="contact.html">Connect with Me</a></li>
         </ul>
+        <div id="footmainbox">
+            <div id="footbox1" class="footsubbox">
+                <a href="https://www.linkedin.com/in/soutrikbanerjee" target="_blank"><img class="socialicons" src="SVGs/linkedin.svg" alt="LinkedIn"></a>
+                <a href="https://www.github.com/soutrikbanerjee-official" target="_blank"><img class="socialicons" src="SVGs/github.svg" alt="GitHub"></a>
+                <a href="https://www.youtube.com/@soutrikbanerjee-official" target="_blank"><img class="socialicons" src="SVGs/youtube.svg" alt="YouTube"></a>
+            </div>
+            <div id="footbox2" class="footsubbox"><p style="font-size: 1rem;" class="footnavlink">Made with ❤︎ in India</p></div>
+            <div id="footbox3" class="footsubbox"><a style="font-size: 1rem;" class="footnavlink" href="https://github.com/soutrikbanerjee-official/personal-website" target="_blank">Click here for Source Code</a></div>
+        </div>
         <div>
             <p>&copy; 2026 Soutrik Banerjee. All rights reserved.</p>
-        </div>`;
+        </div>
+`
+;
