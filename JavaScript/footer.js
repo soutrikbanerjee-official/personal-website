c1bbcb140a8a5c102babc25717bb70461b500c41
Soutrik Banerjee <150548253+soutrikbanerjee-official@users.noreply.github.com>
2026-08-06 12:13:20 +0000

    Update 26.0.17

---
+++ JavaScript/footer.js
@@ -0,0 +1,9 @@
+document.getElementById("footer").innerHTML = `<ul id="footernav">
+            <li class="footnavlink"><a class="footnavlink" href="index.html">Home</a></li>
+            <li class="footnavlink"><a class="footnavlink" href="about.html">About Me</a></li>
+            <li class="footnavlink"><a class="footnavlink" href="projects.html">Projects</a></li>
+            <li class="footnavlink"><a class="footnavlink" href="contact.html">Connect with Me</a></li>
+        </ul>
+        <div>
+            <p>&copy; 2026 Soutrik Banerjee. All rights reserved.</p>
+        </div>`;
