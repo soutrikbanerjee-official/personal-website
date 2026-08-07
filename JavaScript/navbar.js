6a22587a1f89fcc076e4debd6f4841867f550e44
Soutrik Banerjee <150548253+soutrikbanerjee-official@users.noreply.github.com>
2026-08-07 17:09:02 +0000

    Version 26.1

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -4,12 +4,3 @@ document.getElementById("header").innerHTML = `<nav id="navbar">
             <a class="navlink" href="projects.html">Projects</a>
             <a class="navlink" href="contact.html">Connect with Me</a>
         </nav>`;
-
-const currentPage = window.location.pathname.split("/").pop() || "index.html";
-const navLinks = document.querySelectorAll('.navlink');
-
-navLinks.forEach(link => {
-    if (link.getAttribute('href') === currentPage) {
-        link.classList.add('active');
-    }
-});
