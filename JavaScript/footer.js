066a86e9afe5f520576ffc71c56a8346f57a6366
Soutrik Banerjee <150548253+soutrikbanerjee-official@users.noreply.github.com>
2026-08-07 20:12:37 +0000

    Optimized UX Flow

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -12,11 +12,11 @@ document.getElementById("footer").innerHTML =
                 <a href="https://www.github.com/soutrikbanerjee-official" target="_blank"><img class="socialicons" src="SVGs/github.svg" alt="GitHub"></a>
                 <a href="https://www.youtube.com/@soutrikbanerjee-official" target="_blank"><img class="socialicons" src="SVGs/youtube.svg" alt="YouTube"></a>
             </div>
-            <div id="footbox2" class="footsubbox"><p style="font-size: 1rem;" class="footnavlink">Made with ❤︎ in India</p></div>
-            <div id="footbox3" class="footsubbox"><a style="font-size: 1rem;" class="footnavlink" href="https://github.com/soutrikbanerjee-official/personal-website" target="_blank">Click here for Source Code</a></div>
+            <div id="footbox2" class="footsubbox"><p style="font-size: 1.25rem;" class="footnavlink">Made with ❤︎ in India</p></div>
+            <div id="footbox3" class="footsubbox"><a style="font-size: 1.25rem;" class="footnavlink" href="https://github.com/soutrikbanerjee-official/personal-website" target="_blank">Website Source Code</a></div>
         </div>
         <div>
-            <p>&copy; 2026 Soutrik Banerjee. All rights reserved.</p>
+            <p id="copyright">&copy; 2026 Soutrik Banerjee. All rights reserved.</p>
         </div>
 `
 ;
