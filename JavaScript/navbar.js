aaf8af2dc9b9ab228976725bff296c7a80b94b3f
Soutrik Banerjee <150548253+soutrikbanerjee-official@users.noreply.github.com>
2026-08-08 10:39:01 +0000

    Optimized UX Flow

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,6 +1,24 @@
-document.getElementById("header").innerHTML = `<nav id="navbar">
+document.getElementById("header").innerHTML =
+`
+<nav id="navbar">
+        <div class="navbrandmobile">Menu</div>
+        <button id="menutoggle" aria-label="Toggle navigation">
+            <span class="hamburger"></span>
+        </button>
+        <div id="nav-menu">
             <a class="navlink" href="index.html">Home</a>
             <a class="navlink" href="about.html">About Me</a>
             <a class="navlink" href="projects.html">Projects</a>
             <a class="navlink" href="contact.html">Connect with Me</a>
-        </nav>`;
+        </div>
+    </nav>
+`
+;
+
+const toggleBtn = document.getElementById("menutoggle");
+const navMenu = document.getElementById("nav-menu");
+
+toggleBtn.addEventListener("click", () => {
+    toggleBtn.classList.toggle("open");
+    navMenu.classList.toggle("open");
+});
